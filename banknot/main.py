3fdeb7724a38a6b24a7b474ffbf805a66423ea25
celilileri <celil.ileri@bil.omu.edu.tr>
2016-10-27 23:31:38 +0300

    Banknot verici

---
+++ banknot/main.py
@@ -0,0 +1,27 @@
+#5 Lira ve Katı Parayı En Az Banknotla Veren Algoritma Denemesi
+BANKNOTLAR=[0,0,0,0,0,0] #[5,10,20,50,100,200]
+def BanknotSay(para):
+    if para%5 !=0:
+        print("5 Tl ve Katlarını yazabilirsiniz.")
+        exit()
+    if para >= 200:
+        BANKNOTLAR[5] = int(para/200)
+        para %= 200
+    if para >= 100:
+        BANKNOTLAR[4] = int(para/100)
+        para %= 100
+    if para >= 50:
+        BANKNOTLAR[3] = int(para/50)
+        para %= 50
+    if para >= 20:
+        BANKNOTLAR[2] = int(para/20)
+        para %= 20
+    if para >= 10:
+        BANKNOTLAR[1] = int(para/10)
+        para %= 10
+    if para >= 5:
+        BANKNOTLAR[0] = int(para/5)
+        para %= 5
+    return para
+BanknotSay(385)
+print(BANKNOTLAR)
